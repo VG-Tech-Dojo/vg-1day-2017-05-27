fb2c060835070f5804a7ed63fa87ab2c33732446
yukimine <Yukimine_Hayashi@voyagegroup.com>
2017-05-18 22:47:44 +0900

    :sparkles: Mission2-2の解答

--- original/bot/processor.go
+++ original/bot/processor.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"net/url"
 	"regexp"
 	"strings"
 
@@ -11,6 +12,7 @@ import (
 
 const (
 	keywordApiUrlFormat = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=%s&sentence=%s&output=json"
+	talkApiUrl          = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk"
 )
 
 type (
@@ -27,6 +29,9 @@ type (
 
 	// メッセージ本文からキーワードを抽出するprocessorの構造体です
 	KeywordProcessor struct{}
+
+	// メッセージ本文に対する雑談応答を作るprocessorの構造体です
+	TalkProcessor struct{}
 )
 
 // Process は"hello, world!"というbodyがセットされたメッセージのポインタを返します
@@ -70,5 +75,28 @@ func (p *KeywordProcessor) Process(msgIn *model.Message) *model.Message {
 
 	return &model.Message{
 		Body: "キーワード：" + strings.Join(keywords, ", "),
+	}
+}
+
+// Process はメッセージ本文に対する雑談応答を作ります
+func (p *TalkProcessor) Process(msgIn *model.Message) *model.Message {
+	r := regexp.MustCompile("\\Atalk (.*)\\z")
+	matchedStrings := r.FindStringSubmatch(msgIn.Body)
+	text := matchedStrings[1]
+
+	params := url.Values{
+		"apikey": {env.TalkApiKey},
+		"query":  {text},
+	}
+
+	var json interface{}
+
+	post(talkApiUrl, params, &json)
+
+	result := json.(map[string]interface{})["results"]
+	reply := result.([]interface{})[0].(map[string]interface{})["reply"]
+
+	return &model.Message{
+		Body: reply.(string),
 	}
 }
