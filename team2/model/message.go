9e605f09e54c2d2075be1a5b518021d24cb210a1
KazukiSakurada <sk.bumpbaske@gmail.com>
2017-05-27 17:59:51 +0900

    [WIP]チーム2

--- team2/model/message.go
+++ team2/model/message.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 // Message はメッセージの構造体です
@@ -9,14 +10,14 @@ type Message struct {
 	ID   int64  `json:"id"`
 	Body string `json:"body"`
 	Username string `json:"username"`
-	// 1-1. ユーザー名を表示しよう
+	Country string `json:"country"`
 }
 
 // MessagesAll は全てのメッセージを返します
 func MessagesAll(db *sql.DB) ([]*Message, error) {
 
 	// 1-1. ユーザー名を表示しよう
-	rows, err := db.Query(`select id, body, username from message`)
+	rows, err := db.Query(`select id, body, username, country from message`)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +27,7 @@ func MessagesAll(db *sql.DB) ([]*Message, error) {
 	for rows.Next() {
 		m := &Message{}
 		// 1-1. ユーザー名を表示しよう
-		if err := rows.Scan(&m.ID, &m.Body, &m.Username); err != nil {
+		if err := rows.Scan(&m.ID, &m.Body, &m.Username, &m.Country); err != nil {
 			return nil, err
 		}
 		ms = append(ms, m)
@@ -43,7 +44,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 	m := &Message{}
 
 	// 1-1. ユーザー名を表示しよう
-	if err := db.QueryRow(`select id, body from message where id = ?`, id).Scan(&m.ID, &m.Body, &m.Username); err != nil {
+	if err := db.QueryRow(`select id, body from message where id = ?`, id).Scan(&m.ID, &m.Body, &m.Username, &m.Country); err != nil {
 		return nil, err
 	}
 
@@ -53,7 +54,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 // Insert はmessageテーブルに新規データを1件追加します
 func (m *Message) Insert(db *sql.DB) (*Message, error) {
 	// 1-2. ユーザー名を追加しよう
-	res, err := db.Exec(`insert into message (body, username) values (?, ?)`, m.Body, m.Username)
+	res, err := db.Exec(`insert into message (body, username, country) values (?, ?, ?)`, m.Body, m.Username, m.Country)
 	if err != nil {
 		return nil, err
 	}
@@ -66,6 +67,7 @@ func (m *Message) Insert(db *sql.DB) (*Message, error) {
 		ID:   id,
 		Body: m.Body,
 		Username: m.Username,
+		Country: m.Country,
 	}, nil
 }
 
