5297a55d8d1f13c281ecdcac88d1a15f2f68689f
name <email>
2017-05-27 14:03:02 +0900

    misson1-2 commit

--- mizuki/model/message.go
+++ mizuki/model/message.go
@@ -6,9 +6,9 @@ import (
 
 // Message はメッセージの構造体です
 type Message struct {
-	ID       int64  `json:"id"`
-	Body     string `json:"body"`
-	UserName string `json:"username"`
+	ID   int64  `json:"id"`
+	Body string `json:"body"`
+	Username string `json:"username"`
 	// 1-1. ユーザー名を表示しよう
 }
 
@@ -16,7 +16,7 @@ type Message struct {
 func MessagesAll(db *sql.DB) ([]*Message, error) {
 
 	// 1-1. ユーザー名を表示しよう
-	rows, err := db.Query(`select id, body,username from message`)
+	rows, err := db.Query(`select id, body, username from message`)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +26,7 @@ func MessagesAll(db *sql.DB) ([]*Message, error) {
 	for rows.Next() {
 		m := &Message{}
 		// 1-1. ユーザー名を表示しよう
-		if err := rows.Scan(&m.ID, &m.Body, &m.UserName); err != nil {
+		if err := rows.Scan(&m.ID, &m.Body, &m.Username); err != nil {
 			return nil, err
 		}
 		ms = append(ms, m)
@@ -43,7 +43,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 	m := &Message{}
 
 	// 1-1. ユーザー名を表示しよう
-	if err := db.QueryRow(`select id, body from message where id = ?`, id).Scan(&m.ID, &m.Body); err != nil {
+	if err := db.QueryRow(`select id, body, username from message where id = ?`, id).Scan(&m.ID, &m.Body, &m.Username); err != nil {
 		return nil, err
 	}
 
@@ -53,9 +53,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 // Insert はmessageテーブルに新規データを1件追加します
 func (m *Message) Insert(db *sql.DB) (*Message, error) {
 	// 1-2. ユーザー名を追加しよう
-	//res, err := db.Exec(`insert into message (body) values (?)`, m.Body)
-	res, err := db.Exec(`insert into message (body, username) values (?,?)`, m.Body, m.UserName)
-
+	res, err := db.Exec(`insert into message (body) values (?)`, m.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -65,9 +63,8 @@ func (m *Message) Insert(db *sql.DB) (*Message, error) {
 	}
 
 	return &Message{
-		ID:       id,
-		Body:     m.Body,
-		UserName: m.UserName,
+		ID:   id,
+		Body: m.Body,
 		// 1-2. ユーザー名を追加しよう
 	}, nil
 }
--- yuta/model/message.go
+++ yuta/model/message.go
@@ -6,9 +6,8 @@ import (
 
 // Message はメッセージの構造体です
 type Message struct {
-	ID       int64  `json:"id"`
-	Body     string `json:"body"`
-	UserName string `json:"username"`
+	ID   int64  `json:"id"`
+	Body string `json:"body"`
 	// 1-1. ユーザー名を表示しよう
 }
 
@@ -16,7 +15,7 @@ type Message struct {
 func MessagesAll(db *sql.DB) ([]*Message, error) {
 
 	// 1-1. ユーザー名を表示しよう
-	rows, err := db.Query(`select id, body,username from message`)
+	rows, err := db.Query(`select id, body from message`)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +25,7 @@ func MessagesAll(db *sql.DB) ([]*Message, error) {
 	for rows.Next() {
 		m := &Message{}
 		// 1-1. ユーザー名を表示しよう
-		if err := rows.Scan(&m.ID, &m.Body, &m.UserName); err != nil {
+		if err := rows.Scan(&m.ID, &m.Body); err != nil {
 			return nil, err
 		}
 		ms = append(ms, m)
@@ -53,9 +52,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 // Insert はmessageテーブルに新規データを1件追加します
 func (m *Message) Insert(db *sql.DB) (*Message, error) {
 	// 1-2. ユーザー名を追加しよう
-	//res, err := db.Exec(`insert into message (body) values (?)`, m.Body)
-	res, err := db.Exec(`insert into message (body, username) values (?,?)`, m.Body, m.UserName)
-
+	res, err := db.Exec(`insert into message (body) values (?)`, m.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -65,9 +62,8 @@ func (m *Message) Insert(db *sql.DB) (*Message, error) {
 	}
 
 	return &Message{
-		ID:       id,
-		Body:     m.Body,
-		UserName: m.UserName,
+		ID:   id,
+		Body: m.Body,
 		// 1-2. ユーザー名を追加しよう
 	}, nil
 }
